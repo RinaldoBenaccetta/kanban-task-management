6762d63c4497c5657dfe8162dcc868ab4c0fe7d1
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-11-15 11:46:42 +0100

    Rename prepopulateDatabase.js to prepopulateDB.js

--- backend/routes/prepopulate.js
+++ backend/routes/prepopulate.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const prepopulateDatabase = require('../controllers/prepopulateDatabase')
+const prepopulateDatabase = require('../controllers/prepopulateDB')
 
 module.exports = async function (fastify, opts) {
     fastify.get('/api/prepopulate', async function (request, reply) {
