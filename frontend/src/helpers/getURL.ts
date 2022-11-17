ac66036719ba65148905af01fbc45e43aa60ca84
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-11-17 12:30:22 +0100

    Add doc to getURL.ts

--- frontend/src/helpers/getURL.ts
+++ frontend/src/helpers/getURL.ts
@@ -1,3 +1,8 @@
+/**
+ * Returns the base URL of the page.
+ * There is to call the backend API that will be hosted with another
+ * URL.
+ */
 export default () => {
     const protocol = window.location.protocol
     const host = window.location.host
