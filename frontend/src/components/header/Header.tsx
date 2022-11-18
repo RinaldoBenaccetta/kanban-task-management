76c10493e8bc1e0dd9d22415033731cfd9dc6db6
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-11-18 10:22:36 +0100

    Integrate styled-components

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+// Create a Header component that'll render an <div> tag with some styles
+const Header = styled.div`
+    width: 100%;
+    font-size: 5em;
+    text-align: center;
+    background-color: green;
+`
 
 export default () => {
     return (
-        <div>
+        <Header>
             <div>logo</div>
             <div>actual board</div>
             <div>
                 <button>+ Add new task</button>
             </div>
-        </div>
+        </Header>
     )
 }
