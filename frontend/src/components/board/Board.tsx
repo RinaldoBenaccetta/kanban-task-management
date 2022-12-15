69bc4b272b20412b913a41499a71713664a9103f
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-12-15 14:13:39 +0100

    implement the board container

--- frontend/src/components/board/Board.tsx
+++ frontend/src/components/board/Board.tsx
@@ -4,6 +4,7 @@ import { getBoard } from '../../queries/getBoard'
 import { LoaderFunctionArgs, useLoaderData } from 'react-router-dom'
 import { setBoardData } from '../../features/board/boardSlice'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
+import { BoardContainer } from './styles/Board'
 
 export async function loader({ params }: LoaderFunctionArgs) {
     const board = await getBoard(params.boardId)
@@ -28,7 +29,20 @@ export default () => {
 
     dispatch(setBoardData(board))
 
+    const sidePanel = useAppSelector((state) => state.sidePanel.value)
+    // Retrieve the classes to add based on the value of panelVisibility
+    const boardClass = sidePanel ? 'right' : 'left'
+
     console.log(boardData)
 
-    return <div>Hello</div>
+    return (
+        <BoardContainer className={boardClass}>
+            board
+            <div>
+                column
+                <div>status</div>
+                <div>taskList</div>
+            </div>
+        </BoardContainer>
+    )
 }
---
+++ frontend/src/components/board/styles/Board.tsx
@@ -0,0 +1,46 @@
+import styled from 'styled-components'
+import { PropsThemeType } from '../../../@types/ThemesType'
+import DarkModeTransition from '../../../themes/mixins/DarkModeTransition'
+import BreakPointMixin from '../../../themes/mixins/BreakPointMixin'
+
+export const BoardContainer = styled.div`
+    ${DarkModeTransition};
+
+    background-color: ${(props: PropsThemeType) =>
+        props.theme.colors.frontBackground};
+
+    position: fixed;
+
+    top: 64px;
+
+    width: 100%;
+    height: 100%;
+
+    z-index: -1;
+
+    ${BreakPointMixin.tablet`
+        top: 81px;
+        
+       &.left {
+          transition: background-color 0.5s, transform 0.2s ease-out, color 0.25s, border-color 1s;
+          
+          transform: translateX(0);
+      }
+  
+      &.right {
+          transition: background-color 0.5s, transform 0.2s ease-out, color 0.25s, border-color 1s;
+          
+          transform: translateX(261px);
+      }
+    `};
+
+    ${BreakPointMixin.large`
+        top: 98px;
+        
+      &.right {
+          transition: background-color 0.5s, transform 0.2s ease-out, color 0.25s, border-color 1s;
+          
+          transform: translateX(300px);
+      }
+    `};
+`
