1c15ee798849e8add82f95ab5aa39f72f768cce5
Rinaldo Benaccetta <benaccettarinaldo@gmail.com>
2022-12-20 10:19:16 +0100

    deploy

--- models/connect.js
+++ models/connect.js
@@ -2,13 +2,32 @@ const { connect, set } = require('mongoose')
 const dbSettings = require('./settings/dbSettings')
 
 /**
+ * Get the database address according to the values provided
+ * in the environment variables.
+ * in DB_User or DB_Password are empty or undefined,
+ * the address will be without credentials.
+ *
+ * @returns {`mongodb://${string|string}${string}`|`mongodb://${string|string}`}
+ */
+const getDatabaseAddress = () => {
+    const user = process.env.DB_USER || ''
+    const password = process.env.DB_PASSWORD || ''
+    const host = process.env.DB_HOST || ''
+
+    const credentials = user && password ? `${user}:${password}@` : ''
+
+    return `mongodb://${credentials}${host}/${dbSettings.database}`
+}
+
+/**
  * Connect to the database provided in dbSettings
  * Set debug mode according to debug mode provided in dbSettings
  *
  * @returns {Promise<void>}
  */
 module.exports = async () => {
-    await connect(`mongodb://localhost:27017/${dbSettings.database}`)
+    await connect(getDatabaseAddress())
+    // await connect(`mongodb://localhost:27017/${dbSettings.database}`)
     // use `await mongoose.connect('mongodb://user:password@localhost:27017/test');` if your database has auth enabled
 
     set('debug', dbSettings.debugMode)
